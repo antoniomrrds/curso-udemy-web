dd7fe24d19fb6661ce13a77e70f1a4d3a92630f3
Marcos Antonio <antoniomarcos.amrrds@gmail.com>
2021-01-19 00:52:09 -0300

    parte dos formularios

--- exercicio-web/angular/frontend/src/app/components/product/product-create/product-create.component.ts
+++ exercicio-web/angular/frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,8 +1,7 @@
+import { Product } from './../product.model';
 import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
-
 
 @Component({
   selector: 'app-product-create',
@@ -11,16 +10,27 @@ import { Router } from '@angular/router';
 })
 export class ProductCreateComponent implements OnInit {
 
+  product: Product = {
+    name: '',
+    price: 0
+  }
+
   constructor(private productService: ProductService,
-  private router:Router) { }
+      private router: Router) { }
 
   ngOnInit(): void {
+    
   }
-  
-  createProduct(): void { 
-    this.productService.showMessage('Operação executada com sucesso !!!')
+
+  createProduct(): void {
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Produto criado!')
+      this.router.navigate(['/products'])
+    })
+
   }
-  cancel(): void { 
-    this.productService.showMessage('Operação executada com sucesso !!!')
+
+  cancel(): void {
+    this.router.navigate(['/products'])
   }
 }
